b0051738069dbb39a9293e770b6c410c063fd80e
MARYAM <1221102878@student.mmu.edu.my>
2025-06-25 22:55:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ class App{
         
         const self = this;
         
-        loader.load( './assets/hdr/pond_bridge_night_2k.hdr', ( texture ) => {
+        loader.load( './assets/hdr/night_bridge_2k.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
