b6e4fd1fdf67217f3d6b2f83de186f276d2e01d8
MARYAM <1221102878@student.mmu.edu.my>
2025-06-25 21:50:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import * as THREE from './libs/three/three.module.js';
 import { GLTFLoader } from './libs/three/jsm/GLTFLoader.js';
 import { DRACOLoader } from './libs/three/jsm/DRACOLoader.js';
@@ -29,10 +28,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xfff8e7, 0x333344, 1.2);
- const sun = new THREE.DirectionalLight(0xffffff, 0.5);
-sun.position.set(5, 10, 7.5);
-this.scene.add(sun);
+		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -77,7 +73,7 @@ this.scene.add(sun);
         
         const self = this;
         
-        loader.load( './assets/hdr/pond_bridge_night_2k.hdr', ( texture ) => {
+        loader.load( './assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
