0382784331fc95a8f09b7bd6a81ac280889b3065
MARYAM <1221102878@student.mmu.edu.my>
2025-06-26 17:35:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ class App {
 		const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
 		pmremGenerator.compileEquirectangularShader();
 
-		loader.load('./assets/hdr/venice_sunset_1k.hdr', (texture) => {
+		loader.load('./assets/hdr/bambanani_sunset_1k.hdr', (texture) => {
 			const envMap = pmremGenerator.fromEquirectangular(texture).texture;
 			pmremGenerator.dispose();
 			this.scene.environment = envMap;
