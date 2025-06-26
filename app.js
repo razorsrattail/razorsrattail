eecc5574f3d865c9ea456fa8fbe70aad4d78d0b6
MARYAM <1221102878@student.mmu.edu.my>
2025-06-26 15:57:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -164,13 +164,13 @@ const loadExtraModel = (filename, position, rotationY = 0) => {
 };
 
 // Add RV
-loadExtraModel('BREAKING BAD RV.glb', new THREE.Vector3(5, 0, 10), Math.PI);
+loadExtraModel('BREAKING BAD RV.glb', new THREE.Vector3(0, 0, -2), Math.PI);
 
 // Add Jesse Pinkman
-loadExtraModel('JESSE PINKMAN.glb', new THREE.Vector3(8, 0, -12));
+loadExtraModel('JESSE PINKMAN.glb', new THREE.Vector3(-2, 0, -2));
 
 // Add Walter White
-loadExtraModel('WALTER WHITE.glb', new THREE.Vector3(10, 0, -12));
+loadExtraModel('WALTER WHITE.glb', new THREE.Vector3(2, 0, -2));
 			},
 			// called while loading is progressing
 			function ( xhr ) {
